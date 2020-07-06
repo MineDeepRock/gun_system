a128749d68bcb460eff4f07e3a197e6586683d4c
suinua <maatinosippo@gmail.com>
2020-07-06 23:05:18 +0900

    たまのエンティティとリスナーを登録

--- src/gun_system/Main.php
+++ src/gun_system/Main.php
@@ -2,10 +2,15 @@
 
 namespace gun_system;
 
+use gun_system\listener\GunListener;
+use gun_system\pmmp\entity\BulletEntity;
+use pocketmine\entity\Entity;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase
 {
     public function onEnable() {
+        Entity::registerEntity(BulletEntity::class, true, ['Egg', 'minecraft:egg']);
+        $this->getServer()->getPluginManager()->registerEvents(new GunListener($this->getScheduler()), $this);
     }
 }
