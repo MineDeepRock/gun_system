aade07a8b6a0a4d5ad786d52c79fdbe3c42c7328
suinua <maatinosippo@gmail.com>
2020-07-06 23:04:29 +0900

    GiveScareServiceを実装

--- src/gun_system/service/GiveScareService.php
+++ src/gun_system/service/GiveScareService.php
@@ -5,15 +5,18 @@ namespace gun_system\service;
 
 
 use Closure;
+use pocketmine\entity\Effect;
+use pocketmine\entity\EffectInstance;
+use pocketmine\Player;
 use pocketmine\scheduler\ClosureTask;
 use pocketmine\scheduler\TaskScheduler;
 
 class GiveScareService
 {
-    static function execute(TaskScheduler $scheduler) {
-        $scheduler->scheduleDelayedTask(new ClosureTask(function (int $tick) : void {
-            if ($this->owner->isOnline()) {
-                //TODO: イベントを送る
+    static function execute(TaskScheduler $scheduler,Player $player) {
+        $scheduler->scheduleDelayedTask(new ClosureTask(function (int $tick) use ($player) : void {
+            if ($player->isOnline()) {
+                $player->addEffect(new EffectInstance(Effect::getEffect(Effect::NIGHT_VISION), 5, 1));
             }
         }), 20 * 5);
     }
