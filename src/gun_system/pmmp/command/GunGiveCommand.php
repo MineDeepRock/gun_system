35c3716ca06418830e701ea52a139bd8b88104f1
suinua <maatinosippo@gmail.com>
2020-07-06 23:05:55 +0900

    コマンドを追加

---
+++ src/gun_system/pmmp/command/GunGiveCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace gun_system\pmmp\command;
+
+
+use gun_system\GunSystem;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\Player;
+
+class GunGiveCommand extends Command
+{
+    public function __construct() {
+        parent::__construct("GunGive", "", "");
+        $this->setPermission("GunGive.Command");
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args) {
+        if (count($args) === 0) {
+            $sender->sendMessage("/gungive name");
+            return true;
+        }
+        if ($sender instanceof Player) {
+            $itemGun = GunSystem::getItemGun($args[0]);
+            $sender->getInventory()->addItem($itemGun);
+            return true;
+        }
+
+
+        return true;
+    }
+}
