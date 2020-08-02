4fcdb8b9026a8c7d5052c0945dbd1541844d1854
suinua <maatinosippo@gmail.com>
2020-08-02 16:07:39 +0900

    update

--- src/gun_system/pmmp/service/GenerateGunDescriptionService.php
+++ src/gun_system/pmmp/service/GenerateGunDescriptionService.php
@@ -30,13 +30,17 @@ class GenerateGunDescriptionService
             $describe .= str_repeat(TextFormat::GREEN . "■", 40);
         }
 
+        $describe .= "(".$gun->getAttackPoint()->getValue().")";
+
         $describe .="\n" . TextFormat::WHITE . "速" ;
         $describe .= str_repeat(TextFormat::GREEN . "■", ceil($gun->getBulletSpeed()->getPerSecondBlock() / 25));
         $describe .= str_repeat(TextFormat::WHITE . "■", 40 - ceil(($gun->getBulletSpeed()->getPerSecondBlock() / 25)));
+        $describe .= "(".$gun->getBulletSpeed()->getPerSecondBlock()."m/s)";
 
         $describe .="\n" . TextFormat::WHITE . "ﾚｰﾄ" ;
         $describe .= str_repeat(TextFormat::GREEN . "■", ceil($gun->getFiringRate()->getPerSecond())*2);
         $describe .= str_repeat(TextFormat::WHITE . "■", 40 - ceil($gun->getFiringRate()->getPerSecond())*2);
+        $describe .= "(".$gun->getFiringRate()->getPerSecond()."毎秒)";
 
         $describe .= "\n" . TextFormat::WHITE . "装弾数:" . $gun->getMagazineData()->getCapacity() . "/" . $gun->getInitialAmmo();
         $describe .= "\n" . TextFormat::WHITE . "リロード:" . $gun->getReloadingData()->toString();
@@ -50,6 +54,7 @@ class GenerateGunDescriptionService
             $describe .= TextFormat::WHITE . "覗" . str_repeat(TextFormat::GREEN . "■", 40 - (100 - ceil($gun->getPrecision()->getADS())));
             $describe .= str_repeat(TextFormat::WHITE . "■", 100 - ceil($gun->getPrecision()->getADS()));
         }
+        $describe .= "(".$gun->getPrecision()->getADS()."%)";
 
         if ($gun->getPrecision()->getHipShooting() <= 60) {
             $describe .= "\n" . TextFormat::WHITE . "腰" . str_repeat(TextFormat::GREEN . "■", 1);
@@ -59,6 +64,7 @@ class GenerateGunDescriptionService
             $describe .= "\n" . TextFormat::WHITE . "腰" . str_repeat(TextFormat::GREEN . "■", 40 - (100 - ceil($gun->getPrecision()->getHipShooting())));
             $describe .= str_repeat(TextFormat::WHITE . "■", 100 - ceil($gun->getPrecision()->getHipShooting()));
         }
+        $describe .= "(".$gun->getPrecision()->getHipShooting()."%)";
 
         return $describe;
     }
