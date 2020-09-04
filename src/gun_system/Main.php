37cfb77afb3cf31f99e3f28fcd78d274a8798d9a
suinua <maatinosippo@gmail.com>
2020-09-04 19:25:03 +0900

    fix

--- src/gun_system/Main.php
+++ src/gun_system/Main.php
@@ -5,7 +5,6 @@ namespace gun_system;
 use gun_system\listener\GunListener;
 use gun_system\pmmp\command\GunGiveCommand;
 use gun_system\pmmp\entity\BulletEntity;
-use gun_system\service\GenerateGunDataListFilesService;
 use pocketmine\entity\Entity;
 use pocketmine\plugin\PluginBase;
 
@@ -13,8 +12,6 @@ class Main extends PluginBase
 {
     public function onEnable() {
         new GunSystem($this->getScheduler());
-
-        GenerateGunDataListFilesService::execute("D:/");
 
         $this->getServer()->getCommandMap()->register("gungive",new GunGiveCommand());
         Entity::registerEntity(BulletEntity::class, true, ['Egg', 'minecraft:egg']);
